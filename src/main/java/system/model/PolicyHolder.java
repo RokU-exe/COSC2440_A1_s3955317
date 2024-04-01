3670680a6da3e52c7b55fa1297e419ebea750830
RokU <104127134+RokU-exe@users.noreply.github.com>
2024-04-01 17:42:08 +0700

    Updated PolicyHolder.java

--- src/main/java/system/model/PolicyHolder.java
+++ src/main/java/system/model/PolicyHolder.java
@@ -4,5 +4,32 @@
 
 package system.model;
 
-public class PolicyHolder {
+import java.util.ArrayList;
+import java.util.List;
+
+public class PolicyHolder extends Customer {
+    private List<Dependent> dependents = new ArrayList<>();
+
+    public PolicyHolder(String id, String fullName, String insuranceCardId) {
+        super(id, fullName, insuranceCardId);
+    }
+
+    public void addDependent(Dependent dependent) {
+        this.dependents.add(dependent);
+    }
+
+    public List<Dependent> getDependents() {
+        return dependents;
+    }
+
+    @Override
+    public String toString() {
+        // Simplified for brevity, adjust as necessary
+        return "PolicyHolder{" +
+                "id='" + id + '\'' +
+                ", fullName='" + fullName + '\'' +
+                ", insuranceCardId='" + insuranceCardId + '\'' +
+                ", dependents=" + dependents +
+                '}';
+    }
 }
